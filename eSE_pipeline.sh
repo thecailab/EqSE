541caa4be71c5861b7897efe83c2bb03b177a037
GuoshuaiCai <caigs.whu@gmail.com>
2023-02-21 16:11:55 -0500

    Update and rename EqSE_pipeline.sh to eSE_pipeline.sh

--- eSE_pipeline.sh
+++ eSE_pipeline.sh
@@ -46,6 +46,6 @@ reachtools bam2Mtx2 H3K27ac_merged_dna_mm10_sorted_rmdup_sorted.bam ROSE/All_H3K
 
 #STEP5
 #conjugating histone modification data into above seurat object
-#perform standard correlation analysis to identify EqSEs (Fig. )
-#integrative analysis and visualization of EqSEs (Fig. )
+#perform standard correlation analysis to identify eSEs (Fig. )
+#integrative analysis and visualization of eSEs (Fig. )
 
